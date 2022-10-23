a560d23f9ae4199833ef442d61c80c158674a599
henry <s-hliao@lwsd.org>
2022-10-23 01:09:02 -0400

    About Us Page completed

--- app/src/main/java/com/example/hackgt22/ui/AboutUsFragment.java
+++ app/src/main/java/com/example/hackgt22/ui/AboutUsFragment.java
@@ -1,66 +1,118 @@
 package com.example.hackgt22.ui;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.hackgt22.ContactUsActivity;
+import com.example.hackgt22.FAQ;
 import com.example.hackgt22.R;
+import com.example.hackgt22.TermsActivity;
+import com.example.hackgt22.databinding.FragmentAboutUsBinding;
+import com.example.hackgt22.databinding.FragmentHelpBinding;
+import com.example.hackgt22.databinding.ItemHelpBinding;
+import com.example.hackgt22.databinding.ItemLinksBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AboutUsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AboutUsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String mTitle[] = {
+            "Georgia Tech",
+            "Facebook",
+            "Instagram",
+            "Twitter"
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    };
+    int images[] = {
+            R.mipmap.gt_logo,
+            R.mipmap.facebook_logo,
+            R.mipmap.instagram_logo,
+            R.mipmap.twitter_logo
+    };
 
-    public AboutUsFragment() {
-        // Required empty public constructor
-    }
+    FragmentAboutUsBinding binding;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutUsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutUsFragment newInstance(String param1, String param2) {
-        AboutUsFragment fragment = new AboutUsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+
+
+        binding = FragmentAboutUsBinding.inflate(inflater, container, false);
+
+        ListView listView = binding.listLinks;
+        final Context c = getContext();
+
+        AboutUsFragment.MyAdapter adapter = new AboutUsFragment.MyAdapter(c, mTitle, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent browserIntent = null;
+                switch (position){
+                    case 0:
+                        browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gatech.edu/"));
+                        break;
+                    case 1:
+                        browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/georgiatech/"));
+                        break;
+                    case 2:
+                        browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/georgiatech/"));
+                        break;
+                    case 3:
+                        browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/GeorgiaTech"));
+                        break;
+                }
+
+                if (browserIntent != null)
+                    startActivity(browserIntent);
+            }
+        });
+        return binding.getRoot();
+
+
+
+
+
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        int rImgs[];
+
+        MyAdapter(Context c, String title[],  int imgs[]) {
+            super(c, R.layout.item_links, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rImgs = imgs;
+        }
+
+        View row;
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            ItemLinksBinding binding = ItemLinksBinding.inflate(layoutInflater, parent, false);
+            binding.linksImage.setImageResource(rImgs[position]);
+            binding.linksMain.setText(rTitle[position]);
+
+            return binding.getRoot();
+        }
     }
 }
