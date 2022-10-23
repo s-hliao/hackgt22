62f69272e7bfbb9fa6e97abb5e35df67e94a1f4c
henry <s-hliao@lwsd.org>
2022-10-23 00:04:15 -0400

    Help page layout + Contact us

---
+++ app/src/main/java/com/example/hackgt22/ContactUsActivity.java
@@ -0,0 +1,86 @@
+package com.example.hackgt22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ContactUsActivity extends AppCompatActivity {
+    EditText subject;
+    EditText message;
+    Button send;
+    Toast myToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+        getSupportActionBar().setTitle("Contact Us");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        subject = findViewById(R.id.editText6);
+        message = findViewById(R.id.editText5);
+        send = findViewById(R.id.button3);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(subject.getText().toString().equals("")){
+                    alert("Needs a subject.");
+                } else if(message.getText().toString().equals("")){
+                    alert("Needs a message.");
+                } else {
+                    sendEmail(message.getText().toString(), subject.getText().toString());
+                }
+
+            }
+        });
+
+    }
+
+    protected void sendEmail(String messageText, String subjectText) {
+
+        String[] TO = {"hliao62@gatech.edu"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subjectText);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, messageText);
+
+        subject.setText("");
+        message.setText("");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            alert("Select Email to Use");
+        } catch (android.content.ActivityNotFoundException ex) {
+            alert("There is no email client installed.");
+        }
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    private void alert(String message) {
+        if(myToast != null)
+            myToast.cancel();
+        myToast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        myToast.show();
+    }
+}
--- app/src/main/java/com/example/hackgt22/ui/HelpFragment.java
+++ app/src/main/java/com/example/hackgt22/ui/HelpFragment.java
@@ -1,66 +1,111 @@
 package com.example.hackgt22.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.example.hackgt22.ContactUsActivity;
+import com.example.hackgt22.FAQ;
 import com.example.hackgt22.R;
+import com.example.hackgt22.TermsActivity;
+import com.example.hackgt22.databinding.FragmentHelpBinding;
+import com.example.hackgt22.databinding.FragmentHomeBinding;
+import com.example.hackgt22.databinding.ItemHelpBinding;
+import com.example.hackgt22.databinding.ItemLinksBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HelpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HelpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private String mTitle[] = {
+            "FAQ",
+            "Contact Us",
+            "Terms of Use and Licensing",
+    };
+    private String mDescription[] = {
+            "Common questions on how to use MyVFarm",
+            "Need additional help?",
+            "Review terms of service"
+    };
+    private int images[] = {
+            R.drawable.ic_baseline_question_mark_24,
+            R.drawable.ic_baseline_chat_24,
+            R.drawable.ic_baseline_text_snippet_24
+    };
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HelpFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HelpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HelpFragment newInstance(String param1, String param2) {
-        HelpFragment fragment = new HelpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentHelpBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+
+        binding = FragmentHelpBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        ListView listView = binding.listHelp;
+        final Context c = getContext();
+
+        HelpFragment.MyAdapter adapter = new HelpFragment.MyAdapter(c, mTitle, mDescription, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    startActivity(new Intent(getActivity(), FAQ.class));
+                } else if (position == 1) {
+                    startActivity(new Intent(getActivity(), ContactUsActivity.class));
+                } else if (position == 2) {
+                    startActivity(new Intent(getActivity(), TermsActivity.class));
+                }
+            }
+        });
+        return root;
     }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter(Context c, String title[], String description[], int imgs[]) {
+            super(c, R.layout.item_links, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+        }
+
+        View row;
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            ItemHelpBinding binding = ItemHelpBinding.inflate(layoutInflater, parent, false);
+
+            binding.helpImage.setImageResource(rImgs[position]);
+            binding.helpMain.setText(rTitle[position]);
+            binding.helpSub.setText(rDescription[position]);
+
+            return binding.getRoot();
+        }
+    }
+
+
+
 }
